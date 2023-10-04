d4c88fcdf97ff1953939c8ea791f382a3f923108
Carol Sivila <carol.sivila@jala.university>
2023-10-04 11:03:29 -0400

    encriptacion

--- src/main/java/jala/university/todo_app/controllers/RegisterController.java
+++ src/main/java/jala/university/todo_app/controllers/RegisterController.java
@@ -1,114 +1,108 @@
 package jala.university.todo_app.controllers;
-import com.mongodb.client.*;
-import com.mongodb.client.result.InsertOneResult;
-import org.bson.Document;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.mongodb.client.*;
+
+import com.mongodb.client.result.InsertOneResult;
+
+import java.util.regex.Matcher;
+
+import java.util.regex.Pattern;
+
+import org.bson.Document;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.control.PasswordField;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.image.ImageView;
+
+import javafx.scene.layout.Pane;
 
 public class RegisterController {
 
-    private static MongoClient mongoClient;
-    private static MongoDatabase database;
+    public static MongoClient mongoClient;
 
-    private static StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+    public static MongoDatabase database;
     @FXML
     private Label appSectionLabel;
-
     @FXML
     private Label appTitleLabel;
-
     @FXML
     private Label loginLinkLabel;
-
     @FXML
     private Label loginQuestionLabel;
-
     @FXML
     private Button registerButton;
-
     @FXML
     private Pane registerFormPane;
-
     @FXML
     private ImageView registerImg;
-
     @FXML
     private Pane registerUser;
-
     @FXML
     private PasswordField repeatPasswordField;
-
     @FXML
     private TextField userEmailField;
-
     @FXML
     private TextField userNameField;
-
     @FXML
     private PasswordField userPasswordField;
-
     @FXML
     private Label nameFieldAlertLabel;
     @FXML
     private Label emailFieldAlertLabel;
     @FXML
     private Label passwordFieldAlertLabel;
-
     @FXML
-    private Label repeatPasswordAlertLabel; //TODO: implementar lógica para verificar la contraseña.
+    private Label repeatPasswordAlertLabel;
     @FXML
     void onRegisterButtonClick() {
-        try{
+        try {
             eraseAlerts();
             mongoClient = MongoClients.create("mongodb+srv://losmakias:losmakias1@cluster0.m1zizil.mongodb.net/?retryWrites=true&w=majority");
             database = mongoClient.getDatabase("ToDoApp");
             MongoCollection<Document> collection = database.getCollection("Usuarios");
-
-            if (fieldValidation()){
-                //TODO: Implementar el resto de validaciones.
-                nameFieldAlertLabel.setText("Este campo no puede estar vacío.");
-                emailFieldAlertLabel.setText("");
-            }else {
-                fieldValidation();
-                encryptor.setPassword(userPasswordField.getText());
-                String userPass = encryptor.encrypt(userPasswordField.getText());
-                Document newUsuario = new Document("nombre", userNameField.getText()).append("email",userEmailField.getText()).append("password", userPass);
-                collection.insertOne(newUsuario);
+            if (fieldValidation() || !validateEmail(userEmailField.getText())) {
+                return;
             }
-
-        }catch (Exception e){
+            Document existingUser = collection.find(new Document("email", userEmailField.getText())).first();
+            if (existingUser != null) {
+                emailFieldAlertLabel.setText("Correo electrónico ya registrado.");
+                return;
+            }
+            String password = userPasswordField.getText();
+            String contra = BCrypt.hashpw(password, BCrypt.gensalt());
+            Document newUsuario = new Document("nombre", userNameField.getText()).append("email", userEmailField.getText()).append("password", contra);
+            System.out.println(contra);
+            InsertOneResult result = collection.insertOne(newUsuario);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-
-    //TODO: agregar redireccionamiento en la app hacia el apartado de login.
-
-    boolean fieldValidation(){
-        //TODO: Hacer el resto de validaciones.
-        if(userNameField.getText().isEmpty()){
+    boolean fieldValidation() {
+        if (userNameField.getText().isEmpty()) {
             System.out.println("name field empty");
             return true;
         }
         return false;
     }
-
-    //TODO: Implementar validación del email.
-    private boolean isValidEmail(String email){
-        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
-        return email.matches(emailRegex);
+    boolean validateEmail(String email) {
+        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
-    void eraseAlerts(){
+    void eraseAlerts() {
         nameFieldAlertLabel.setText("");
     }
-
 }
-
-
