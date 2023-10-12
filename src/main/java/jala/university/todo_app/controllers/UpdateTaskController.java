3b947ed12ad493e27c81c246cb215462347282fa
Carol Sivila <carol.sivila@jala.university>
2023-10-12 10:20:32 -0400

    falta login

--- src/main/java/jala/university/todo_app/controllers/UpdateTaskController.java
+++ src/main/java/jala/university/todo_app/controllers/UpdateTaskController.java
@@ -1,5 +1,6 @@
 package jala.university.todo_app.controllers;
 
+import jala.university.todo_app.DatabaseConnection;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -36,8 +37,15 @@ public class UpdateTaskController {
   private TextField updateTaskTitle;
 
   @FXML
-  void createTask(MouseEvent event) {
+  void updateTask(MouseEvent event) {
+    String taskId = "6525a6518b6b300d4ad24dcb";
+    DatabaseConnection.updateTask(taskId, updateTaskTitle.getText(), updateTask.getText());
 
   }
 
+  @FXML
+  void deleteTask(MouseEvent event) {
+    String taskId = "6525a6518b6b300d4ad24dcb";
+    DatabaseConnection.deleteTask(taskId);
+  }
 }
